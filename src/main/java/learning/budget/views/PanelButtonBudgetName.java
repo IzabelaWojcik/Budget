424d66e2ec27d1c5a97a8c6ae84db352d6fa3561
Iza <iza.wojcik.iw@gmail.com>
2018-07-31 22:51:55 +0200

    Added new panels

---
+++ src/main/java/learning/budget/views/PanelButtonBudgetName.java
@@ -0,0 +1,23 @@
+package learning.budget.views;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class PanelButtonBudgetName extends JPanel{
+	//FIXME its only create buttons, they dont do anything, action listener needed
+	// budgetCurrentId isnt use here
+	public void fillPanelWithBudgetsButtons(HashMap<Integer, String> budgetIdNameMap) {
+		int budgetCurrentId;
+		for (Entry<Integer, String> entry : budgetIdNameMap.entrySet()) {
+			budgetCurrentId = entry.getKey();
+			JButton jButtonBudgetName = new JButton(entry.getValue());
+			add(jButtonBudgetName);
+		}
+		
+		revalidate();
+		repaint();
+	}
+}
---
+++ src/main/java/learning/budget/views/PanelButtonsWithMonths.java
@@ -0,0 +1,22 @@
+package learning.budget.views;
+
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import learning.budget.DataFormatter;
+
+public class PanelButtonsWithMonths extends JPanel{
+//TODO list: check how get month name in localdare
+//listOfMonths = sort.sortMonthsForConcreteYearAndBudgetId(usersIncomeObjectList, budgetId,year);
+	public void fillPanelWithMonthsButtons(ArrayList<Integer> listOfMonths) {
+		DataFormatter dataFormatter = new DataFormatter();
+		for (int i = 0; i < listOfMonths.size(); i++) {
+			int month = listOfMonths.get(i);
+			String monthName = dataFormatter.changeMonhNumberFromMonthName(month);
+			JButton jButtonWithMonthName = new JButton(monthName + "");
+			add(jButtonWithMonthName);
+		}
+	}
+}
